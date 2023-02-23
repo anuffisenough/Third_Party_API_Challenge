abf19b2b6e43855f2b8257bcb482ef7f6d6931a4
anuffisenough <uffmanch@gmail.com>
2023-02-23 14:06:23 -0700

    adds functionality for color-coding timeslots

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
+
+$(function () {  
+  
+  //saves current dayjs time as variable "today"
+var today = dayjs();
+//var hours = [9, 10, 11, 12, 13, 14, 15, 16];
+//sets the display header to show today's date (look into updating every second)********
+    $('#currentDay').text(today.format('MMMM DD, YYYY'));
+  
   //beginning of the input reading function for the event listener
   function handleCalendarEntries() {
-    console.log(event.target);
-    //console.log($(calendarListDiv.children('div').children('textarea')).val());
+    
   }
+  //array of hour block divs to iterate over to set past, current and future stylings
 
-  
+  var hourBlockDivs = $("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13, #hour-14, #hour-15, #hour-16");
+
+  var currentHour = dayjs().format("H");
+    for (let i = 0; i < hourBlockDivs.length; i++) {
+      if (hourBlockDivs[i].dataset.hour < currentHour) {
+        $(hourBlockDivs[i]).addClass("past");
+      } else if (hourBlockDivs[i].dataset.hour === currentHour) {
+        $(hourBlockDivs[i]).addClass("present");
+      } else if (hourBlockDivs[i].dataset.hour > currentHour) {
+        $(hourBlockDivs[i]).addClass("future");
+      };
+    }
+
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -27,12 +44,10 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   
-  //saves current dayjs time as variable "today"
-var today = dayjs();
-//sets the display header to show today's date (look into updating every second)********
-    $('#currentDay').text(today.format('MMMM DD, YYYY'));
+
 
     
 var calendarListDiv = $("#event-list-div");
-    calendarListDiv.on('click', '.btn', handleCalendarEntries);
+    calendarListDiv.on('click', '.saveBtn', handleCalendarEntries);
 });
+
